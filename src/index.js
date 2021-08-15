6a5d43d1efa989c8a5a54adb90fb8ecb0c1a2625
ilona <vpsvest@tut.by>
2021-08-15 17:48:22 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function toReadable (number) {
-  
+    const ones = ['zero','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+    const tens = ['','ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+    if (number < 20){
+    return ones[number];
+    } else if (number < 100 && number % 10 === 0) {
+    	return tens[number / 10]
+    } else if (number < 100) {
+        return tens[Math.floor(number/10)] + ' ' + ones[number % 10];
+    } else if (number % 100 === 0) {
+    	return ones[number / 100] + ' hundred';
+	} else if (number % 10 === 0) {
+    	return ones[Math.floor(number / 100)] + ' hundred ' + tens[number % 100 / 10];	
+    } else if (number % 100 < 20) {
+    	return ones[Math.floor(number / 100)] + ' hundred ' + ones[number % 100];	
+	} else if (number < 1000) {
+		return ones[Math.floor(number/100)] + ' hundred ' + tens[Math.floor(number % 100 / 10)] + ' ' + ones[(number % 100) % 10];
+  }
+return number;
 }
